d5c32c6a96e7a12a4d1ed7e5c4846eceeca2be3a
Mostafa Al Moin <sa479374@gmail.com>
2024-08-24 14:14:50 +0600

    cover screen add

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,6 @@
+import 'package:brick_breaker/coverscreen.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,26 +10,47 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // ball variables
+  double ballX = 0;
+  double ballY = 0;
+
+  // game setting
+  bool hasGameStarted = false;
+
+  void startGame() {
+    hasGameStarted = true;
+    Timer.periodic(const Duration(milliseconds: 10), (timer) {
+      setState(() {
+        ballY -= 0.005;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.deepPurple[100],
-      body: Center(
-        child: Stack(
-          children: [
-            // ball
-            Container(
-              alignment: const Alignment(0, 0),
-              child: Container(
-                height: 15,
-                width: 15,
-                decoration: const BoxDecoration(
-                  color: Colors.deepPurple,
-                  shape: BoxShape.circle,
+    return GestureDetector(
+      onTap: startGame,
+      child: Scaffold(
+        backgroundColor: Colors.deepPurple[100],
+        body: Center(
+          child: Stack(
+            children: [
+              // tap to play
+              CoverScreen(hasGameStarted: hasGameStarted),
+              // ball
+              Container(
+                alignment: Alignment(ballX, ballY),
+                child: Container(
+                  height: 15,
+                  width: 15,
+                  decoration: const BoxDecoration(
+                    color: Colors.deepPurple,
+                    shape: BoxShape.circle,
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
